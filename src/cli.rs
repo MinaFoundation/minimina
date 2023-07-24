8c8ec7f58d5beb34dbeab75c6f49916258dfdcce
Piotr Stachyra <piotr.stachyra@gmail.com>
2023-07-24 16:10:23 +0200

    add basic unit tests

--- src/cli.rs
+++ src/cli.rs
@@ -54,3 +54,134 @@ pub struct NodeId {
     #[clap(short, long)]
     pub node_id: String,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_network_create_command() {
+        let args = vec![
+            "minimina",
+            "network",
+            "create",
+            "--topology",
+            "/path/to/file",
+            "--genesis-ledger",
+            "/path/to/dir",
+            "--network-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Network(NetworkCommand::Create(args)) => {
+                assert_eq!(args.topology, std::path::PathBuf::from("/path/to/file"));
+                assert_eq!(args.genesis_ledger, std::path::PathBuf::from("/path/to/dir"));
+                assert_eq!(args.network_id.network_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+
+    #[test]
+    fn test_network_deploy_command() {
+        let args = vec![
+            "minimina",
+            "network",
+            "deploy",
+            "--network-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Network(NetworkCommand::Deploy(args)) => {
+                assert_eq!(args.network_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+
+    #[test]
+    fn test_network_destroy_command() {
+        let args = vec![
+            "minimina",
+            "network",
+            "destroy",
+            "--network-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Network(NetworkCommand::Destroy(args)) => {
+                assert_eq!(args.network_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+
+    #[test]
+    fn test_node_start_command() {
+        let args = vec![
+            "minimina",
+            "node",
+            "start",
+            "--node-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Node(NodeCommand::Start(args)) => {
+                assert_eq!(args.node_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+
+    #[test]
+    fn test_node_stop_command() {
+        let args = vec![
+            "minimina",
+            "node",
+            "stop",
+            "--node-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Node(NodeCommand::Stop(args)) => {
+                assert_eq!(args.node_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+
+    #[test]
+    fn test_node_logs_command() {
+        let args = vec![
+            "minimina",
+            "node",
+            "logs",
+            "--node-id",
+            "test",
+        ];
+
+        let cli = Cli::parse_from(args);
+
+        match cli.commands {
+            Commands::Node(NodeCommand::Logs(args)) => {
+                assert_eq!(args.node_id, "test");
+            }
+            _ => panic!("Unexpected command parsed"),
+        }
+    }
+}
