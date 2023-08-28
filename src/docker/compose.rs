528df22ff5309b6f1285fb639217de68978aff22
Piotr Stachyra <piotr.stachyra@gmail.com>
2023-08-28 12:43:30 +0200

    divide docker_compose.rs into compose and manager modules and initial generation od docker-compose file

---
+++ src/docker/compose.rs
@@ -0,0 +1,185 @@
+use log::warn;
+use serde::ser::{SerializeStruct, Serializer};
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+use std::{collections::HashMap, path::PathBuf};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum ServiceType {
+    Seed,
+    BlockProducer,
+    SnarkWorker,
+    SnarkCoordinator,
+    ArchiveNode,
+}
+
+#[derive(Debug)]
+pub struct ServiceConfig {
+    pub service_type: ServiceType,
+    pub service_name: String,
+    pub docker_image: String,
+    pub public_key: Option<String>,
+    pub public_key_path: Option<String>,
+    pub libp2p_keypair: Option<String>,
+    pub peers: Option<Vec<String>>,
+    pub client_port: Option<u16>,
+}
+
+impl ServiceConfig {
+    fn generate_seed_command(&self) -> String {
+        assert_eq!(self.service_type, ServiceType::Seed);
+
+        let client_port = self.client_port.unwrap_or(3100);
+        let rest_port = client_port + 1;
+        let external_port = rest_port + 1;
+        let metrics_port = external_port + 1;
+        let libp2p_metrics_port = metrics_port + 1;
+
+        let client_port_str = client_port.to_string();
+        let rest_port_str = rest_port.to_string();
+        let external_port_str = external_port.to_string();
+        let metrics_port_str = metrics_port.to_string();
+        let libp2p_metric_port = libp2p_metrics_port.to_string();
+        let base_command = vec![
+            "daemon",
+            "-client-port",
+            &client_port_str,
+            "-rest-port",
+            &rest_port_str,
+            "-insecure-rest-server",
+            "-external-port",
+            &external_port_str,
+            "-metrics-port",
+            &metrics_port_str,
+            "-libp2p-metrics-port",
+            &libp2p_metric_port,
+            "-config-file",
+            "/local-network/genesis_ledger.json",
+            "-log-json",
+            "-log-level",
+            "Trace",
+            "-file-log-level",
+            "Trace",
+            "-seed",
+        ];
+
+        let libp2p_keypair = match &self.libp2p_keypair {
+            Some(libp2p_keypair) => {
+                let libp2p_keypair = format!("-libp2p-keypair {}", libp2p_keypair);
+                libp2p_keypair
+            }
+            None => {
+                warn!("No libp2p keypair provided for seed node. This is not recommended.");
+                "".to_string()
+            }
+        };
+
+        let config_directory = format!(
+            "-config-directory /local-network/nodes/{}",
+            self.service_name
+        );
+
+        [base_command, vec![&libp2p_keypair, &config_directory]]
+            .concat()
+            .join(" ")
+    }
+}
+
+#[derive(Serialize)]
+pub(crate) struct DockerCompose {
+    version: &'static str,
+    #[serde(
+        rename = "x-defaults",
+        serialize_with = "serialize_defaults_with_anchor"
+    )]
+    x_defaults: Defaults,
+    services: HashMap<String, Service>,
+}
+
+fn serialize_defaults_with_anchor<S>(defaults: &Defaults, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut state = s.serialize_struct("Defaults", 1)?;
+    state.serialize_field("&default-attributes", defaults)?;
+    state.end()
+}
+
+#[derive(Serialize)]
+struct Defaults {
+    network_mode: &'static str,
+    entrypoint: Vec<&'static str>,
+    volumes: Vec<String>,
+    environment: Environment,
+}
+
+#[derive(Serialize)]
+struct Environment {
+    MINA_PRIVKEY_PASS: &'static str,
+    MINA_LIBP2P_PASS: &'static str,
+}
+
+#[derive(Serialize)]
+struct Service {
+    #[serde(rename = "<<", serialize_with = "no_quotes_merge")]
+    merge: &'static str,
+    container_name: String,
+    image: String,
+    command: String,
+}
+
+fn no_quotes_merge<S>(merge: &&'static str, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_str(merge)
+}
+
+impl DockerCompose {
+    pub fn generate(configs: Vec<ServiceConfig>, network_path: &PathBuf) -> String {
+        let networt_path_string = network_path
+            .to_str()
+            .expect("Failed to convert network path to str");
+        let services: HashMap<String, Service> = configs
+            .iter()
+            .map(|config| {
+                let service = Service {
+                    merge: "*default-attributes",
+                    container_name: config.service_name.clone(),
+                    image: config.docker_image.to_string(),
+                    command: match config.service_type {
+                        ServiceType::Seed => config.generate_seed_command(),
+                        _ => String::new(),
+                    },
+                };
+                (config.service_name.clone(), service)
+            })
+            .collect();
+
+        let compose = DockerCompose {
+            version: "3.8",
+            x_defaults: Defaults {
+                network_mode: "host",
+                entrypoint: vec!["mina"],
+                volumes: vec![format!("{}:/local-network", networt_path_string)],
+                environment: Environment {
+                    MINA_PRIVKEY_PASS: "naughty blue worm",
+                    MINA_LIBP2P_PASS: "naughty blue worm",
+                },
+            },
+            services,
+        };
+
+        let yaml_output = serde_yaml::to_string(&compose).unwrap();
+        Self::post_process_yaml(yaml_output)
+    }
+
+    // fix the format of the yaml output
+    fn post_process_yaml(yaml: String) -> String {
+        yaml.replace(
+            "x-defaults:\n  '&default-attributes':",
+            "x-defaults: &default-attributes",
+        )
+        .replace("<<: '*default-attributes'", "<<: *default-attributes")
+    }
+}
--- src/docker/manager.rs
+++ src/docker/manager.rs
@@ -1,33 +1,44 @@
+/// This is the `manager` module for Docker operations.
+///
+/// It provides functionalities for starting, stopping the docker-compose as well as generating it to disk.
 use std::path::{Path, PathBuf};
 
 use crate::cmd::run_command;
 use std::fs::File;
 use std::io::Write;
 
-pub struct DockerCompose {
+use super::compose::{DockerCompose, ServiceConfig};
+
+pub struct DockerManager {
     pub network_path: PathBuf,
     pub docker_compose_path: PathBuf,
 }
 
-impl DockerCompose {
+impl DockerManager {
     pub fn new(network_path: &Path) -> Self {
         let docker_compose_path = network_path.join("docker-compose.yaml");
-        DockerCompose {
+        DockerManager {
             network_path: network_path.to_path_buf(),
             docker_compose_path,
         }
     }
 
-    pub fn _generate_docker_compose(&self) -> std::io::Result<()> {
-        // TODO: Implement actual generation logic based on topology
-        let contents = "version: '3.5'\nservices:\n  block-producer:\n    image: gcr.io/o1labs-192920/mina-daemon:2.0.0rampup3-bfd1009-buster-berkeley\n";
-
+    pub fn compose_generate_file(&self, configs: Vec<ServiceConfig>) -> std::io::Result<()> {
         let mut file = File::create(&self.docker_compose_path)?;
+        let contents = DockerCompose::generate(configs, &self.network_path);
         file.write_all(contents.as_bytes())?;
         Ok(())
     }
 
-    pub fn run_docker_compose(&self, subcommands: &[&str]) -> std::io::Result<()> {
+    pub fn compose_up(&self) -> std::io::Result<()> {
+        self.run_docker_compose(&["up", "-d"])
+    }
+
+    pub fn compose_down(&self) -> std::io::Result<()> {
+        self.run_docker_compose(&["down", "-d"])
+    }
+
+    fn run_docker_compose(&self, subcommands: &[&str]) -> std::io::Result<()> {
         let network_id = self
             .network_path
             .file_name()
